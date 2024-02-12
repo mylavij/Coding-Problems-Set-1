d19b5b33a8a9b7672fcb34455bd7bd3683c59eac
mylavij <mvijayan@stu.socsd.org>
2024-02-12 14:59:42 -0500

    yy

---
+++ examplethree.java
@@ -0,0 +1,7 @@
+public class examplethree
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Myla Vijayan\nSong: \"always an angel never a god\"\nPoem: \"you do not just wake up and become the butterfly\"");
+    }
+}
---
+++ exampletwo.java
@@ -0,0 +1,7 @@
+public class exampletwo
+{
+    public static void main (String[] args)
+    {
+        System.out.println("****************************************\n*                                      *\n*                                      *\n*            Sue\'s Swimwear            *\n*        SwimWithSue@swimwear.com      *\n*                                      *\n*                                      *\n****************************************");
+    }
+}
